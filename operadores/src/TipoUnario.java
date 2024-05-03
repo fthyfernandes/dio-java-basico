752fdd5ac1ef6f308e50c53f4d206ba449a1e7bd
Thyago Fernandes <thyago.academy@gmail.com>
2024-05-03 11:59:20 -0300

    Exemplos de tipos unários

---
+++ operadores/src/TipoUnario.java
@@ -0,0 +1,25 @@
+public class TipoUnario {
+    public static void main(String[] args) {
+
+        int numero = 5;
+
+        //decrementos e incrementos tbm se encaixam nesse tópico
+
+        numero = +numero;
+        System.out.println(numero);
+
+        numero = -numero;
+        System.out.println(numero);
+
+        numero= -5;
+        System.out.println(numero);
+
+        numero = numero *-1;
+        System.out.println(numero);
+
+        numero = -5 * (-1);
+        System.out.println(numero);
+        
+    }
+    
+}
