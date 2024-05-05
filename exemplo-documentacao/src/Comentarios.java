3d897ff462bccfa43f1d59eed2230d9b1cefa7ea
Thyago Fernandes <thyago.academy@gmail.com>
2024-05-05 12:13:17 -0300

    Exemplos de comentários

---
+++ exemplo-documentacao/src/Comentarios.java
@@ -0,0 +1,25 @@
+public class Comentarios {
+
+    // Olá, eu sou um comentário em uma única linha
+
+
+    /* Olá,
+ * Eu sou um comentario
+ * que posso ser mais detalhadod
+ * quando necessário
+ */
+
+
+
+/** 
+ * Estas duas estrelinhas acima
+ * é para identificar que você
+ * pretende elaborar um comentário
+ * a nível de documentação.
+ * Que incrível !!!
+ */
+ public static void metodo1(){
+
+ }
+    
+}
